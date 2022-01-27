68e03201edbcd87ba63240442d6c8e09b7116526
Marat Gumerov <gumerovm74@gmail.com>
2022-01-27 15:26:33 +0300

    add code

--- weather.js
+++ weather.js
@@ -1,17 +1,52 @@
 #!/ust/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { getWeather, getIcon } from "./services/api.servise.js";
 import { printHelp, printSuccess, printError } from './services/log.service.js'
-import { saveKeyValue } from "./services/storage.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from "./services/storage.service.js";
 
 
 const saveToken = async (token) => {
+    if (!token.length){
+        printError('Не передан token')
+        return;
+    }
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess("Токен сохранен");
-    } catch (e){
-        console.log(e);
+    } catch(e){
         printError(e.message);
     }
+}
+
+const saveCity = async (ciry) => {
+    if (!ciry.length){
+        printError('Не передан город')
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess("Город сохранен");
+    } catch(e){
+        printError(e.message);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon))
+    } catch (e) {
+        if (e?.response?.status == 404){
+            printError('Неверно указан город');
+        } else if (e?.response?.status == 401) {
+            printError('Неверно указан токен');
+        } else {
+            printError(e.message)
+        }
+        
+    }
+    
 }
 
 
@@ -20,10 +55,11 @@ const initCLI = () => {
     console.log(args);
     if (args.h) {
         // Вывод help
-        printHelp();
+        return printHelp();
     }
 
     if (args.s) {
+        return saveCity(args.s);
         // Сохранить город
     }
 
@@ -31,8 +67,8 @@ const initCLI = () => {
         //сохранить токен
         return saveToken('token', args.t)
     }
-
     //вывести погоду
+    return getForcast();
 }
 
 initCLI();
